a6e1ac960ec4856fb3c1da58ac0c653b90564706
Daniel C <emicardenas828@gmail.com>
2025-12-18 23:16:18 -0600

    cross figure

---
+++ cruz.java
@@ -0,0 +1,44 @@
+package cruz;
+import java.util.Scanner;
+public class cruz{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("dame la altura de la cruz: ");
+        int altura = scanner.nextInt();  
+        scanner.close();
+
+        if(altura%2==0){
+            altura +=1;
+        }
+
+        int a = 0;
+
+        while(a<altura/2){
+            for(int i = 0; i < altura-1; i++){
+                System.out.print(" ");
+
+        }
+            System.out.println("*");
+            a++;
+
+
+
+        }
+        for(int i=0; i <altura; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        
+
+        a = 0;
+        while(a<altura/2){
+            for(int i = 0; i < altura-1; i++){
+                System.out.print(" ");
+
+        }
+            System.out.println("*");
+            a++;
+
+        }
+    }
+}
